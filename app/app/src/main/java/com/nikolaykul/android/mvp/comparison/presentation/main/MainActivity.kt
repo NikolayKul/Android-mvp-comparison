2e90f17d7c57745a621d91e7a597e5191d09c78e
Nikolay Kulachenko <gn1kolaykul@gmail.com>
2018-01-28 00:50:29 +0300

    Add loading

--- app/app/src/main/java/com/nikolaykul/android/mvp/comparison/presentation/main/MainActivity.kt
+++ app/app/src/main/java/com/nikolaykul/android/mvp/comparison/presentation/main/MainActivity.kt
@@ -2,16 +2,21 @@ package com.nikolaykul.android.mvp.comparison.presentation.main
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import com.nikolaykul.android.mvp.comparison.R
+
+private const val LOADING_MILLIS = 500L
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var btnLogin: Button
     private lateinit var etUsername: EditText
     private lateinit var etPassword: EditText
+    private lateinit var vgLoading: ViewGroup
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,11 +25,20 @@ class MainActivity : AppCompatActivity() {
         initListeners()
     }
 
+    private fun showCredentials(username: String, password: String) {
+        val msg = "Username: $username, password: $password"
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+    }
+
     private fun initListeners() {
         btnLogin.setOnClickListener {
-            val username = etUsername.text.toString()
-            val password = etPassword.text.toString()
-            showCredentials(username, password)
+            vgLoading.visibility = View.VISIBLE
+            vgLoading.postDelayed({
+                vgLoading.visibility = View.GONE
+                val username = etUsername.text.toString()
+                val password = etPassword.text.toString()
+                showCredentials(username, password)
+            }, LOADING_MILLIS)
         }
     }
 
@@ -33,11 +47,7 @@ class MainActivity : AppCompatActivity() {
         btnLogin = findViewById(R.id.btnLogin)
         etUsername = findViewById(R.id.etUsername)
         etPassword = findViewById(R.id.etPassword)
-    }
-
-    private fun showCredentials(username: String, password: String) {
-        val msg = "Username: $username, password: $password"
-        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+        vgLoading = findViewById(R.id.vgLoading)
     }
 
 }
